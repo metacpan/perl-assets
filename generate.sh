94b8d39e419cf303da8a12f8c478ba758e965c90
Thibault Duponchelle <thibault.duponchelle@gmail.com>
2025-04-25 08:09:42 +0200

    Auto generate a favicon

--- generate.sh
+++ generate.sh
@@ -1,7 +1,9 @@
 cd blessed/src/
 for f in `ls *.svg | sed s/\.svg//g`; do 
 	for s in 64 300 800 1500 2500; do 
-		rsvg-convert -f svg -w $s $f.svg > ../exports/perl-$f-$s.svg;
-		rsvg-convert -f png -w $s $f.svg > ../exports/perl-$f-$s.png;
+		rsvg-convert -f svg -w $s -h $s $f.svg > ../exports/perl-$f-$s.svg;
+		rsvg-convert -f png -w $s -h $s $f.svg > ../exports/perl-$f-$s.png;
 	done
 done
+
+convert -background transparent -define 'icon:auto-resize=16,24,32,64' 097.svg ../exports/favicon.ico
